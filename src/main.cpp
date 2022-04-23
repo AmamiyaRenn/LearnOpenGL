e15e973b918699567ae2b3fa61d52dfc2a1f6dbb
AmamiyaRenn <379617892@qq.com>
2022-04-23 19:21:03 +0800

    master

--- src/main.cpp
+++ src/main.cpp
@@ -13,22 +13,6 @@
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
-const char *vertexShaderSource = "#version 460 core\n"
-                                 "layout(location = 0) in vec3 aPos;\n"
-                                 "layout(location = 1) in vec3 aColor;\n"
-                                 "out vec3 ourColor;\n"
-                                 "void main() {\n"
-                                 "	gl_Position = vec4(aPos, 1.0f);\n"
-                                 "  ourColor = aColor;\n"
-                                 "}\0";
-
-const char *fragmentShaderSource = "#version 460 core\n"
-                                   "out vec4 FragColor;\n"
-                                   "in vec3 ourColor;\n"
-                                   "void main() {\n"
-                                   "	FragColor = vec4(ourColor, 1.0f);\n"
-                                   "}\0";
-
 int main()
 {
     // 1. glfwSetting
